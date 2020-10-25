d9849e70ec79284bb2cc753b817ac058a1e73e05
arjun-707 <ajunakki707@gmail.com>
2020-10-26 04:29:57 +0530

    hacker news component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.scss';
-// import Count from './components/counter/Count';
+import Count from './components/counter/Count';
 import HackerNews from './components/hacker-news/HackerNews';
 
 function App() {
   return (
     <div className="App">
-      {/* <Count /> */}
+      <Count />
       <HackerNews />
     </div>
   );
